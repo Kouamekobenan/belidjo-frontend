6a8ad46672437202fe7fe9038db27c58bc5c8da8
Nelson Kouame <kouamenelson47@gmail.com>
2026-01-01 19:48:00 +0100

    add api url railway

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:3000",
-  // baseURL: "https://belidjo-production.up.railway.app",
+  // baseURL: "http://127.0.0.1:3000",
+  baseURL: "https://belidjo-production.up.railway.app",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
-
